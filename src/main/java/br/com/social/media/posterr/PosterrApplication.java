6b53d35a8a6ed47d50eeeb4808119931c8e53f4d
Gabriela <gabrielacordovaa@hotmail.com>
2022-07-18 21:33:17 -0300

    inicio do projeto

---
+++ src/main/java/br/com/social/media/posterr/PosterrApplication.java
@@ -0,0 +1,13 @@
+package br.com.social.media.posterr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosterrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosterrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/social/media/posterr/PosterrApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.social.media.posterr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosterrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
