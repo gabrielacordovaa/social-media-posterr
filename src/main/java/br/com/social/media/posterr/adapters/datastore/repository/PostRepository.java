495a9545a6254b5876e874480c880d2a030231f1
Gabriela <gabrielacordovaa@hotmail.com>
2022-07-19 23:17:34 -0300

    post queries logics

--- src/main/java/br/com/social/media/posterr/adapters/datastore/repository/PostRepository.java
+++ src/main/java/br/com/social/media/posterr/adapters/datastore/repository/PostRepository.java
@@ -5,9 +5,20 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Date;
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, String> {
 
-    //todo: check if query works later
+    //todo: check
     @Query(value = "SELECT count(1) FROM posts p WHERE p.user.id = :id AND postDate = CURRENT_DATE()", nativeQuery = true)
-    public Integer getUserDailyPublication(@Param("id") String id);
+    Integer getUserDailyPublication(@Param("id") String id);
+
+    //todo: check
+    @Query(value = "SELECT * FROM posts p WHERE p.postDate BETWEEN (CASE WHEN :begin IS NULL THEN p.postDate ELSE :begin END) AND " +
+            "(CASE WHEN :end IS NULL THEN p.postDate ELSE :end END)", nativeQuery = true)
+    List<Post> getPostByDateRange(@Param("begin") Date begin, @Param("end") Date end);
+
+    @Query(value = "SELECT * FROM posts p WHERE p.user.id = :userId", nativeQuery = true)
+    List<Post> getPostsByUserId(@Param("userId") String id);
 }
